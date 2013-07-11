4822106ec99edb2a6836e58cac93e56ed5288fb2
Hakan Oklay <hakanoklay@gmail.com>
2013-07-11 17:40:11 +0000

    Reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
+   
+var text = fs.readFileSync('/index.html','utf8');
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
