9a9b6e192613b312b4fffdfdf47cbf61e3d701a8
Hakan Oklay <hakanoklay@gmail.com>
2013-07-11 18:42:03 +0000

    update 2

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
    
-var text = fs.readFileSync('/index.html','utf8');
+var text = fs.readFileSync('index.html','utf8');
 app.get('/', function(request, response) {
   response.send(' ' + text);
 });
